e6d62472ea47f502e96b24924b76908a05597fdb
Jacqueline Williamson <jacqueline_25@hotmail.com>
2020-08-21 08:55:23 -0400

    numbers display

--- main.js
+++ main.js
@@ -1,5 +1,35 @@
 
 
+const number1 = document.querySelector(".number1") 
+number1.addEventListener ('click', function () {output.textContent += '1'})
 
-let number1 = document.querySelector(".number1") 
-number1.addEventListener ("click", function(){answer.textContent += "1" })
+const number2 = document.querySelector(".number2") 
+number2.addEventListener ('click', function () {output.textContent += '2'})
+
+const number3 = document.querySelector(".number3") 
+number3.addEventListener ('click', function () {output.textContent += '3'})
+
+const number4 = document.querySelector(".number4") 
+number4.addEventListener ('click', function () {output.textContent += '4'})
+
+const number5 = document.querySelector(".number5") 
+number5.addEventListener ('click', function () {output.textContent += '5'})
+
+const number6 = document.querySelector(".number6") 
+number6.addEventListener ('click', function () {output.textContent += '6'})
+
+const number7 = document.querySelector(".number7") 
+number7.addEventListener ('click', function () {output.textContent += '7'})
+
+const number8 = document.querySelector(".number8") 
+number8.addEventListener ('click', function () {output.textContent += '8'})
+
+const number9 = document.querySelector(".number9") 
+number9.addEventListener ('click', function () {output.textContent += '9'})
+
+const number0 = document.querySelector(".number0") 
+number0.addEventListener ('click', function () {output.textContent += '0'})
+
+const decimal = document.querySelector(".decimal") 
+decimal.addEventListener ('click', function () {output.textContent += '.'})
+
